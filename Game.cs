61bd521c7536aef80bc69926ac3ca512034c98f9
kipclass1337 <37634515+kipclass1337@users.noreply.github.com>
2018-03-21 14:52:13 -0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class Game
+    {
+        public String team1, team2;
+        public int team1Score, team2Score;
+
+        public Game()
+        {
+
+        }
+
+        public Game(String team1, String team2, int team1Score, int team2Score)
+        {
+            this.team1 = team1;
+            this.team2 = team2;
+            this.team1Score = team1Score;
+            this.team2Score = team2Score;
+        }
+
+        public override string ToString()
+        {
+            return team1 + "(" + team1Score + ") -- " + team2 + "(" + team2Score + ")";
+        }
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class GameFactory
+    {
+        List<Game> gameList = new List<Game>();
+
+        public void CreateGameList()
+        {
+            Game gamey;
+            gamey = new Game("Celtics", "Rockets", 78, 90);
+            gameList.Add(gamey);
+
+            gamey = new Game("Red Sox", "Yankees", 5, 9);
+            gameList.Add(gamey);
+
+            gamey = new Game("Patriots", "Seahawks", 21, 28);
+            gameList.Add(gamey);
+
+            gamey = new Game("Dolphins", "Raiders", 49, 0);
+            gameList.Add(gamey);
+
+            gamey = new Game("Hurricanes", "Panthers", 36, 70);
+            gameList.Add(gamey);
+
+            gamey = new Game("Jaguars", "Vikings", 14, 9);
+            gameList.Add(gamey);
+        }
+    }
+}
